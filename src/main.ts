321a56828c5da9889acab9ac3637e60500a69d35
masonnguyen1243 <meowjcisgod@gmail.com>
2025-10-06 13:53:13 +0700

    update swagger

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -24,6 +25,16 @@ async function bootstrap() {
     preflightContinue: false,
     credentials: true,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('The Fire Monkey')
+    .setDescription('The Fire Monkey API description')
+    .setVersion('1.0')
+    .addTag('The Fire Monkey')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, documentFactory);
 
   await app.listen(port);
 }
